c66044719829040c035b656b093fba0565f93c91
Ravina Sarda <ravina.sarda@gmail.com>
2024-01-06 12:43:07 +0900

    Linked List Algo

---
+++ LinkedList/index.js
@@ -0,0 +1,110 @@
+function Node(elem){
+    this.value = elem;
+    this.next = null;
+}
+
+
+function LinkedList(){
+    this.head = 0;
+    this.value=null;
+    this.size = 0;
+
+    this.isEmpty=function(){
+        return this.size === 0 ;
+    }
+
+    this.getSize= function(){
+        return this.size;
+    }
+
+    this.prepend=function(elem){
+        let node = new Node(elem);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size++;
+    }
+
+    this.append=function(elem){
+        let node = new Node(elem)
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            let prev = this.head;
+            while(prev.next){
+                prev = prev.next;
+            }
+            prev.next = node;
+        }
+        this.size++;
+    }
+
+    this.print=function(){
+        if(this.isEmpty()){
+            console.log( 'the list is empty')
+        }else{
+            let current = this.head;
+            let listValues='';
+            while(current){
+                listValues += `${current.value} `
+                current = current.next;
+            }
+
+            console.log(listValues);
+        }
+    }
+
+    this.insert=function(value , index){
+        if(index < 0 || index > this.size){
+            return 'index is invalid';
+        }
+        if( index===0){
+            this.prepend(value);
+        }else{
+            let node = new Node(value);
+            let prev = this.head;
+            for(let i=0;i<index-1;i++){
+                prev=prev.next
+            }
+            node.next = prev.next;
+            prev.next = node;
+            this.size++;
+            
+        }
+    }
+
+    this.remove=function(index , value){
+        if(index<0 || index> this.size){
+            return null;
+        }
+        let removeNode;
+        if(index===0){
+            removeNode = this.head;
+            this.head = this.head.next;
+        }else{
+            let prev = this.head;
+            for(let i=0;i<index-1;i++){
+                prev = prev.next;
+
+            }
+            removeNode = prev.next;
+            prev.next = removeNode.next;
+        }
+        this.size--;
+        return removeNode.value;
+    }
+    
+}
+
+let list = new LinkedList();
+console.log(list.isEmpty());
+
+list.append(2);
+list.append(3);
+list.append(4);
+list.insert(8,2);
+list.remove(2)
+console.log(list.print());
